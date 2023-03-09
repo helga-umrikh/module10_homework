b8af77064e6d9ab11bb21342df7bb2229e1d07a6
helga-umrikh <zetaumr@gmail.com>
2023-03-09 20:39:44 +0300

    completed task_1 js css html

--- task_1/task_1.js
+++ task_1/task_1.js
@@ -1,1 +1,6 @@
-Сверстайте кнопку, которая будет содержать в себе icon_01 (как в примере в последнем видео). При клике на кнопку иконка должна меняться на icon_02. Повторный клик меняет иконку обратно.
+const btn = document.querySelector('.j-btn-test');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('hide');
+    btn.classList.toggle('display');
+});
